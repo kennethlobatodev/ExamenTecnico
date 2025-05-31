0ae8bacaa2768bc718b43517e326f73f33be2c7d
kennethlobatodev <55170908+kennethlobatodev@users.noreply.github.com>
2025-05-31 15:13:40 -0600

    Initial project version

---
+++ ExamenTecnico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenTecnico
+//
+//  Created by Kenneth Lobato on 31/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
